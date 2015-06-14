b3fedfaf2d9b0532f66922eb88ea884c5cfd75fc
albert <lomoalbert@gmail.com>
2015-06-14 20:33:24 +0800

    init

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "io/ioutil"
     "encoding/xml"
     "os"
+    "flag"
 )
 
 type bingimage struct {
@@ -18,7 +19,7 @@ type image struct{
     UrlBase string  `xml:"urlBase"`
 }
 
-func getimg(url string){
+func getimg(url,finame string){
     response,err:=http.Get(url)
     if err!=nil{
         fmt.Println(err.Error())
@@ -26,7 +27,6 @@ func getimg(url string){
     defer response.Body.Close()
     fd,err :=ioutil.ReadAll(response.Body)
 
-    finame := "/home/albert/Pictures/wallpaper"
     fi, err := os.OpenFile(finame, os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
         panic(err)
@@ -37,6 +37,12 @@ func getimg(url string){
 }
 
 func main(){
+    finame := flag.String("path", "", "path to backgroud picture.")
+    flag.Parse()
+    if *finame==""{
+        fmt.Println("请输入路径.")
+        return
+    }
     url:="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=8"
     response,err:=http.Get(url)
     if err!=nil{
@@ -48,5 +54,5 @@ func main(){
     err =xml.Unmarshal(fd, &todayimg)
     imgurl:="https://bing.com"+todayimg.Images[0].Url
     fmt.Println(imgurl)
-    getimg(imgurl)
+    getimg(imgurl,*finame)
 }
